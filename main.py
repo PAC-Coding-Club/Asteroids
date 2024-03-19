3a81d6982efd9fcc737bd7dfc51d1aef39440f72
MeltedButetr77 <MeltedButterGaming@gmail.com>
2024-03-19 20:36:21 +1030

    Refactor player movement and drawing in main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((1024, 768))
 
 sprites = pygame.sprite.Group()
-
 player = objects.Player((200, 200), sprites)
 
 running = True
@@ -22,8 +21,6 @@ while running:
     player.update(keys_pressed)
 
     screen.fill("grey")
-    pygame.draw.rect(screen, "green", player.rect)
-    pygame.draw.line(screen, "black", player.rect.center, (player.rect.center[0] + 50 * math.cos(player.angle), player.rect.center[1] + 50 * math.sin(player.angle)))
     sprites.draw(screen)
     pygame.display.update()
 
